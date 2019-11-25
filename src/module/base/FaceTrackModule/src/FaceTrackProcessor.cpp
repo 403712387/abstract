1610c8ea5894a9ec357babce732b3e321db0845e
zhongpanpan <pandazhong@gmail.com>
2019-11-25 20:20:59 +0800

    修复了CPU使用率过高的问题

--- src/module/base/FaceTrackModule/src/FaceTrackProcessor.cpp
+++ src/module/base/FaceTrackModule/src/FaceTrackProcessor.cpp
@@ -84,7 +84,7 @@ std::shared_ptr<Error> FaceTrackProcessor::workThread()
     LOG_I(mClassName, "begin face track processor thread, track info:" << mTrackCondition->toString() << ", face id:" << mFaceId);
     while(!isStop() || !mMessageQueue.empty())
     {
-        std::shared_ptr<BaseMessage> message = mMessageQueue.popExpiration(10);
+        std::shared_ptr<BaseMessage> message = mMessageQueue.popExpiration(40);
         if (NULL == message.get())
         {
             // 检查高质量人脸队列
